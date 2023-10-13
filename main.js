4d5949cfbd4caeef883425b86c40c001c336d23a
nastyaganieva <nastyafreehugs@gmail.com>
2023-10-13 22:12:01 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-const userName = prompt('What is your name?');
-alert('Hello, ' + userName + '! How are you?');
+// const userName = prompt('What is your name?');
+// alert('Hello, ' + userName + '! How are you?');
+
+let firstStr = prompt('Enter the first line');
+let result = alert(`${firstStr} ${secondStr} ${thirdStr}`);
