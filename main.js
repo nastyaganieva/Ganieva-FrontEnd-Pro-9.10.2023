c7f68e1a3477d4436603f0ad635fb1254550b603
nastyaganieva <nastyafreehugs@gmail.com>
2023-10-16 15:28:20 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 // const userName = prompt('What is your name?');
 // alert('Hello, ' + userName + '! How are you?');
 
-let firstStr = prompt('Enter the first line');
-let result = alert(`${firstStr} ${secondStr} ${thirdStr}`);
+// let firstStr = prompt('Enter the first line');
+// let secondStr = prompt('Enter the first line');
+// let thirdStr = prompt('Enter the first line');
+
+// let result = alert(`${firstStr} ${secondStr} ${thirdStr}`);
+
+
+
+const firstValue = +prompt('Enter the first value');
+const secondValue = +prompt('Enter the second value');
+
+alert(`${firstValue} + ${secondValue} = ${firstValue + secondValue}`);
+// alert(firstValue + ' + ' + secondValue + ' = ' + (firstValue + secondValue));
+alert(`${firstValue} - ${secondValue} = ${firstValue - secondValue}`);
+// alert(firstValue + ' - ' + secondValue + ' = ' + (firstValue - secondValue));
+alert(`${firstValue} * ${secondValue} = ${firstValue * secondValue}`);
+// alert(firstValue + ' * ' + secondValue + ' = ' + (firstValue * secondValue));
+alert(`${firstValue} / ${secondValue} = ${firstValue / secondValue}`);
+// alert(firstValue + ' / ' + secondValue + ' = ' + (firstValue / secondValue));
+
+
+
+
+// const hours = prompt('');
+// const seconds = hours * 3600;
+
+// alert(`${seconds} seconds`);
